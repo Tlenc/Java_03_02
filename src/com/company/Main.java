e7cb0fbc8952bbcf2ef74229e29545df95fad8ac
Tlenc <tlenciauskas@gmail.com>
2018-01-31 11:30:43 +0200

    While ciklas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String zodis = "";
+        while (!zodis.equals("pabaiga")) {
+            zodis = sc.next();
+            System.out.println(zodis);
+        }
+
     }
 }
